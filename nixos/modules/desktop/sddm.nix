9cc2db79b0eb10451d795b72e46d418968967e72
Azealea <auhubert6@gmail.com>
2025-05-31 00:04:57 +0200

    feat: sddm corners theming

--- nixos/modules/desktop/sddm.nix
+++ nixos/modules/desktop/sddm.nix
@@ -1,13 +1,9 @@
 {pkgs, ... }:
-
 {
-	services.displayManager.sddm = {
-		enable = true;
-		wayland.enable = true;
-#		theme = "${import ./sddm-theme.nix { inherit pkgs; }}";
-		theme = "${import ./sddm-theme.nix { inherit pkgs;}}";
-
-
-	};
-	services.xserver.enable = true;
+    services.displayManager.defaultSession = "hyprland";
+    services.displayManager.sddm = {
+            enable = true;
+            wayland.enable = true;
+            theme = "${import ./sddm-theme.nix { inherit pkgs;}}";
+    };
 }
