d1720c1a36c00718193d523250f19768fcbd6277
Azealea <auhubert6@gmail.com>
2025-06-17 00:12:53 +0200

    feat: began customizing wofi

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -2,12 +2,12 @@
   imports = [
     ./cursor.nix
     ./git.nix
-    ./wofi.nix
 #   ./qt.nix
     ./hyprland
     ./waybar
     ./zsh.nix
      ./vim.nix
      ./kitty.nix
+     ./wofi/wofi.nix
   ];
 }
---
+++ home-manager/modules/wofi/wofi.nix
@@ -0,0 +1,18 @@
+{
+    programs.wofi = {
+    enable = true;
+    settings = {
+      show = "drun";
+      # width = 800;
+      # height = 500;
+      # location = "center";
+      # halign = "fill";
+      # orientation = "horizontal";
+      # allow_markup = true;
+      # filter_rate = 100;
+      # prompt = "Search";
+      # hide_scroll = false;
+    };
+    style = builtins.readFile ./style.css;
+  };
+}
