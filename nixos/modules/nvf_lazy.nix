e93f37230c9ee08cfef06fcf287492460593a589
Azealea <auhubert6@gmail.com>
2026-02-05 21:04:02 +0100

    fix: nvim config + rmed unecessary fluff

--- nixos/modules/nvf_lazy.nix
+++ nixos/modules/nvf_lazy.nix
@@ -13,11 +13,6 @@
         expandtab = false;
       };
 
-      debugMode = {
-        enable = true;
-        logFile = "/tmp/nvim.log";
-      };
-
       theme = {
         enable = true;
 
@@ -30,23 +25,14 @@
         noice.enable = true;
         colorizer.enable = true;
         illuminate.enable = true;
-
-        breadcrumbs = {
-          enable = false;
-          navbuddy.enable = false;
-        };
-
         smartcolumn.enable = true;
       };
 
       visuals = {
         nvim-web-devicons.enable = true;
         nvim-cursorline.enable = true;
-
         indent-blankline.enable = true;
       };
-
-      notify.nvim-notify.enable = true;
 
       dashboard = {
         alpha.enable = true;
@@ -55,36 +41,13 @@
       statusline.lualine.enable = true;
       tabline.nvimBufferline.enable = true;
 
-      filetree.neo-tree.enable = true;
       telescope.enable = true;
-
-      binds = {
-        whichKey = {
-          enable = true;
-          setupOpts = {
-          };
-        };
-      };
-
-      comments.comment-nvim.enable = true;
-      autopairs.nvim-autopairs.enable = true;
 
       lsp = {
         enable = true;
         formatOnSave = true;
-
-        lspkind.enable = true;
-
         trouble.enable = true;
-
-        lspSignature.enable = false;
       };
-
-      autocomplete = {
-        nvim-cmp.enable = true;
-      };
-
-      snippets.luasnip.enable = true;
 
       languages = {
         enableTreesitter = true;
@@ -94,8 +57,6 @@
         nix.enable = true;
         clang.enable = true;
       };
-
-      treesitter.context.enable = true;
 
       git = {
         enable = true;
@@ -108,20 +69,6 @@
       debugger.nvim-dap = {
         enable = true;
         ui.enable = true;
-      };
-
-      minimap.codewindow.enable = true;
-
-      utility = {
-        ccc.enable = false;
-        diffview-nvim.enable = false;
-        icon-picker.enable = false;
-        leetcode-nvim.enable = false;
-        smart-splits.enable = false;
-
-        surround.enable = true;
-
-        multicursors.enable = true;
       };
     };
   };
--- nixos/modules/nvf_lazy_maps.nix
+++ nixos/modules/nvf_lazy_maps.nix
@@ -7,19 +7,6 @@
     };
 
     keymaps = [
-      # --- Basics ---
-      {
-        mode = "n";
-        key = "<Esc>";
-        action = "<cmd>noh<cr><esc>";
-        desc = "Clear search highlight";
-      }
-      {
-        mode = ["n" "i"];
-        key = "<C-s>";
-        action = "<cmd>w<cr>";
-        desc = "Save file";
-      }
       {
         mode = "n";
         key = "<leader>Q";
@@ -27,124 +14,26 @@
         desc = "Quit all";
       }
 
-      # --- Explorer (neo-tree) ---
-      {
-        mode = "n";
-        key = "<leader>e";
-        action = "<cmd>Neotree toggle<cr>";
-        desc = "Explorer";
-      }
-
-      {
-        mode = "n";
-        key = "<leader>ff";
-        action = "<cmd>Telescope find_files<cr>";
-        desc = "Find files";
-      }
-      {
-        mode = "n";
-        key = "<leader>fg";
-        action = "<cmd>Telescope live_grep<cr>";
-        desc = "Grep (live)";
-      }
-      {
-        mode = "n";
-        key = "<leader>fb";
-        action = "<cmd>Telescope buffers<cr>";
-        desc = "Buffers";
-      }
-      {
-        mode = "n";
-        key = "<leader>fr";
-        action = "<cmd>Telescope oldfiles<cr>";
-        desc = "Recent files";
-      }
-      {
-        mode = "n";
-        key = "<leader>fh";
-        action = "<cmd>Telescope help_tags<cr>";
-        desc = "Help";
-      }
-
-      # --- Buffers (bufferline-ish feel) ---
+      # # Buffers
       {
         mode = "n";
         key = "<S-l>";
-        action = "<cmd>BufferLineCycleNext<cr>";
+        action = "<cmd>bn<cr>";
         desc = "Next buffer";
       }
       {
         mode = "n";
         key = "<S-h>";
-        action = "<cmd>BufferLineCyclePrev<cr>";
+        action = "<cmd>bp<cr>";
         desc = "Prev buffer";
       }
       {
         mode = "n";
-        key = "<leader>bd";
-        action = "<cmd>bdelete<cr>";
+        key = "<M-d>";
+        action = "<cmd>bd<cr>";
         desc = "Delete buffer";
       }
-      {
-        mode = "n";
-        key = "<C-h>";
-        action = "<C-w>h";
-        desc = "Go to left window";
-      }
-      {
-        mode = "n";
-        key = "<C-j>";
-        action = "<C-w>j";
-        desc = "Go to lower window";
-      }
-      {
-        mode = "n";
-        key = "<C-k>";
-        action = "<C-w>k";
-        desc = "Go to upper window";
-      }
-      {
-        mode = "n";
-        key = "<C-l>";
-        action = "<C-w>l";
-        desc = "Go to right window";
-      }
-      {
-        mode = "n";
-        key = "]d";
-        action = "<cmd>lua vim.diagnostic.goto_next()<cr>";
-        desc = "Next diagnostic";
-      }
-      {
-        mode = "n";
-        key = "[d";
-        action = "<cmd>lua vim.diagnostic.goto_prev()<cr>";
-        desc = "Prev diagnostic";
-      }
-      {
-        mode = "n";
-        key = "<leader>cd";
-        action = "<cmd>lua vim.diagnostic.open_float()<cr>";
-        desc = "Line diagnostics";
-      }
-      {
-        mode = "n";
-        key = "<leader>xx";
-        action = "<cmd>Trouble diagnostics toggle<cr>";
-        desc = "Trouble: diagnostics";
-      }
-      {
-        mode = "n";
-        key = "<leader>xq";
-        action = "<cmd>Trouble qflist toggle<cr>";
-        desc = "Trouble: quickfix";
-      }
-      {
-        mode = "n";
-        key = "<leader>xl";
-        action = "<cmd>Trouble loclist toggle<cr>";
-        desc = "Trouble: loclist";
-      }
+      # # lsp
       {
         mode = "n";
         key = "gd";
@@ -195,12 +84,47 @@
       }
       {
         mode = "n";
-        key = "<leader>cf";
-        action = "<cmd>lua vim.lsp.buf.format({ async = true })<cr>";
-        desc = "Format";
+        key = "<leader>x";
+        action = "<cmd>Trouble diagnostics toggle<cr>";
+        desc = "Toggle Trouble";
       }
-
-      # --- Git (gitsigns) ---
+      #      {
+      #        mode = "n";
+      #        key = "]x";
+      #        action = "<cmd>TroubleNext<cr>";
+      #        desc = "Next Trouble item";
+      #      }
+      #      {
+      #        mode = "n";
+      #        key = "[x";
+      #        action = "<cmd>TroublePrevious<cr>";
+      #        desc = "Previous Trouble item";
+      #      }
+      # # git
+      {
+        mode = "n";
+        key = "hs";
+        action = "<cmd>Gitsigns stage_hunk<cr>";
+        desc = "Stage hunk";
+      }
+      {
+        mode = "n";
+        key = "hu";
+        action = "<cmd>Gitsigns undo_stage_hunk<cr>";
+        desc = "Undo stage hunk";
+      }
+      {
+        mode = "n";
+        key = "hr";
+        action = "<cmd>Gitsigns reset_hunk<cr>";
+        desc = "Reset hunk";
+      }
+      {
+        mode = "n";
+        key = "hd";
+        action = "<cmd>Gitsigns preview_hunk_inline<cr>";
+        desc = "Diff hunk";
+      }
       {
         mode = "n";
         key = "]h";
@@ -211,81 +135,7 @@
         mode = "n";
         key = "[h";
         action = "<cmd>Gitsigns prev_hunk<cr>";
-        desc = "Prev hunk";
-      }
-      {
-        mode = "n";
-        key = "<leader>gp";
-        action = "<cmd>Gitsigns preview_hunk<cr>";
-        desc = "Preview hunk";
-      }
-      {
-        mode = "n";
-        key = "<leader>gs";
-        action = "<cmd>Gitsigns stage_hunk<cr>";
-        desc = "Stage hunk";
-      }
-      {
-        mode = "n";
-        key = "<leader>gr";
-        action = "<cmd>Gitsigns reset_hunk<cr>";
-        desc = "Reset hunk";
-      }
-      {
-        mode = "n";
-        key = "<leader>gb";
-        action = "<cmd>Gitsigns blame_line<cr>";
-        desc = "Blame line";
-      }
-
-      # --- DAP / DAP UI ---
-      {
-        mode = "n";
-        key = "<leader>dc";
-        action = "<cmd>DapContinue<cr>";
-        desc = "Debug: continue";
-      }
-      {
-        mode = "n";
-        key = "<leader>db";
-        action = "<cmd>DapToggleBreakpoint<cr>";
-        desc = "Debug: breakpoint";
-      }
-      {
-        mode = "n";
-        key = "<leader>di";
-        action = "<cmd>DapStepInto<cr>";
-        desc = "Debug: step into";
-      }
-      {
-        mode = "n";
-        key = "<leader>do";
-        action = "<cmd>DapStepOver<cr>";
-        desc = "Debug: step over";
-      }
-      {
-        mode = "n";
-        key = "<leader>dO";
-        action = "<cmd>DapStepOut<cr>";
-        desc = "Debug: step out";
-      }
-      {
-        mode = "n";
-        key = "<leader>dr";
-        action = "<cmd>DapToggleRepl<cr>";
-        desc = "Debug: REPL";
-      }
-      {
-        mode = "n";
-        key = "<leader>du";
-        action = "<cmd>DapUiToggle<cr>";
-        desc = "Debug: UI";
-      }
-      {
-        mode = ["n" "v"];
-        key = "<C-M-d>";
-        action = "<cmd>MCstart<cr>";
-        desc = "MultiCursor: start (VSCode Ctrl+D style)";
+        desc = "Previous hunk";
       }
     ];
   };
